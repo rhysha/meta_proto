862f3345537bfa8a489a768489dcda930ef0ec62
rhysha <rhysha.bd@gmail.com>
2022-04-06 12:59:16 +0600

    changes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
-const epubParser = require('epub-metadata-parser');
-const ebookConverter = require('node-ebook-converter');
+
+
 var zmq = require("zeromq"),
   sock = zmq.socket("push");
 
@@ -24,45 +24,32 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true, parameterLimit: 1000000 }))
 app.use(morgan('dev'));
-app.get('/', function (request, response) {
-  epubParser.parse("./epub/The-Secret-Adversary.epub", './doc/', book => {
-    console.log(book);
-  });
-});
+
 app.post('/upload', async (req, res) => {
- // try {
-    //console.log(req.files)
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: 'No file uploaded'
-      });
-    } else {
-      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-      let file = req.files.book;
-
-      //Use the mv() method to place the file in upload directory (i.e. "uploads")
-      file.mv('./uploads/' + file.name);
-      sock.send(file.name);
-      res.send({
-        stauts:true,
-        message: "Book Uploaded"
-      })
-      // await ebookConverter.convert({
-      //   input: "./uploads/" + file.name,
-      //   output: "./epub/" + file.name + ".epub"
-      // }).then(response => console.log(response))
-      //   .catch(error => console.error(error));
-
-
-      // epubParser.parse("./epub/" + file.name + ".epub", './doc/', book => res.json(book));
-      //send response
-     
-    }
-  // } catch (err) {
-  //   res.status(500).send(err);
-  // }
+  // try {
+  //console.log(req.files)
+  if (!req.files) {
+    res.send({
+      status: false,
+      message: 'No file uploaded'
+    });
+  } else {
+    let file = req.files.book;
+    file.mv('./uploads/' + file.name);
+    sock.send(file.name);
+    res.send({
+      stauts: true,
+      message: "Successfull"
+    })
+  }
 });
+
+app.post('/search', async (req, res) => {
+  // try {
+  //console.log(req.files)
+  
+});
+
 var port = process.env.PORT || 5003;
 
 app.listen(port, function () {
--- worker.js
+++ worker.js
@@ -1,10 +1,45 @@
 // worker.js
 var zmq = require("zeromq"),
   sock = zmq.socket("pull");
-
+var MongoClient = require('mongodb').MongoClient;
+const ebookConverter = require('node-ebook-converter');
+const epubParser = require('epub-metadata-parser');
+f = require('util').format,
 sock.connect("tcp://127.0.0.1:3001");
 console.log("Worker connected to port 3000");
+var user = 'citizix';
+var password = 'S3cret';
+var authMechanism = 'DEFAULT';
+var url = f('mongodb://%s:%s@localhost:27017',
+  user, password);
 
-sock.on("message", function(msg) {
+var MongoClient = require('mongodb').MongoClient;
+
+
+sock.on("message", async function (msg) {
   console.log("work: %s", msg.toString());
+  // convertion
+  await ebookConverter.convert({
+    input: "./uploads/" + msg.toString(),
+    output: "./epub/" + msg.toString() + ".epub"
+  }).then(response => console.log("CONVERTING"))
+    .catch(error => console.error("ERROR",error));
+  // parse
+  epubParser.parse("./epub/"+msg.toString()+".epub" , './doc/', book => {
+    console.log(book.author,book.title,book.subject,book.language,book.pubdate,book.fileName);
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("elib");
+      var bookJson = { author: book.author, title: book.title, subject:book.subject, language:book.language, pubdate: book.pubdate,filename: book.fileName};
+      dbo.collection("books").insertOne(bookJson, function(err, res) {
+        if (err) throw err;
+        console.log("1 book inserted");
+        db.close();
+      });
+    });
+  });
+  // meta extraction
+  
+  // save in db
+
 });
