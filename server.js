5882617b1ec6893fcc2fd3ddc304c3f0e9167997
rhysha <rhysha.bd@gmail.com>
2022-04-06 16:35:53 +0600

    more modification

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-var express = require("express");
+const express = require("express");
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
-var nodepub = require('nodepub');
+const nodepub = require('nodepub');
 f = require('util').format
-var user = 'citizix';
-var password = 'S3cret';
-var url = f('mongodb://%s:%s@localhost:27017',
-  user, password);
+const user = 'citizix';
+const password = 'S3cret';
+const url = f('mongodb://%s:%s@localhost:27017',user, password);
 
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const { exec } = require("child_process");  
-var zmq = require("zeromq"),
+const zmq = require("zeromq"),
   sock = zmq.socket("push");
 
 sock.bindSync("tcp://127.0.0.1:3001");
 console.log("Producer bound to port 3000");
 
-var app = express();
+const app = express();
 //app.use(express.logger());
 // enable files upload
 app.use(fileUpload({
@@ -68,15 +67,42 @@ app.get('/book', async (req, res) => {
 app.post('/book/:id/meta', async (req, res) => {
   // try {
   //console.log(req.files)
-  let cmd = f("ebook-meta %s -t %s","epub/Alice-in-Wonderland.pdf.epub","Hello World")
-  exec(cmd, (error, stdout, stderr) => {});
-  
-  res.json({status:true})
+  let bookId = req.params.id
+  const ObjectId = require('mongodb').ObjectId; 
+  const o_id = new ObjectId(bookId);
+  const client = new MongoClient(url);
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db("elib");
+  const collection = db.collection('books');
+  console.log(bookId)
+  const findResult = await collection.find({_id:o_id}).toArray();
+  console.log(findResult[0]['filename']);
+  let title = req.body.title
+  let author = req.body.author
+  let cover = req.body.cover
+  let publisher = req.body.publisher
+  let isbn = req.body.isbn
+  let tags = req.body.tags
+
+
+  let cmd = f("ebook-meta ./epub/%s --title %s --authors=%s --cover=%s  --publisher=%s --isbn=%s --tags=%s",
+  findResult[0]['filename'],
+  "sdasdasdsa",
+  "rhysha",
+  "test.jpg",
+  "PentaGlobal",
+  "id:859432",
+  "world,hello")
+  console.log(cmd)
+  await exec(cmd, (error, stdout, stderr) => {console.log(stdout)});
+
+  res.json({status:true,result:"Meta Updated for "+findResult[0]['filename']})
 });
 
 
 
-var port = process.env.PORT || 5003;
+const port = process.env.PORT || 5003;
 
 app.listen(port, function () {
   console.log("Listening on " + port);
